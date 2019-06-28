45b335cb69fa0e7eb1f2103440dafaed583aae42
Paul-Pi <342130397@qq.com>
2019-06-28 10:22:02 +0800

    添加@Accessor注解

--- sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
+++ sp01-commons/src/main/java/com/tedu/sp01/pojo/Item.java
@@ -3,8 +3,10 @@ package com.tedu.sp01.pojo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
 
 @Data
+@Accessors(chain= true)
 @NoArgsConstructor
 @AllArgsConstructor
 public class Item {
